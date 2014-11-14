6c3102aa8628b23cb108ab2bc127e12d85000423
John Hu <huziyong@gmail.com>
2014-11-14 22:26:11 +0800

    Config username to model and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,30 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessor :login
+
+  validates :username,
+    :uniqueness => {
+      :case_sensitive => false
+    },
+    :format => { with: /\A[a-zA-Z]+[a-zA-Z0-9]+\z/,
+    message: "begin with letters and only allows letters or digits"  }
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      else
+        where(conditions).first
+      end
+    end
 end
