c0be0fd37d6fed9a402d165cfa6f2e13619d40b8
John Hu <huziyong@gmail.com>
2014-11-15 21:48:41 +0800

    Create idea

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,8 +1,34 @@
 class IdeasController < ApplicationController
+  before_action :get_idea, only: [:show, :edit, :update, :destroy]
+
   def index
+  end
+
+  def show
   end
 
   def new
     @idea = Idea.new
   end
+
+  def create
+    @idea = current_user.ideas.build(idea_params)
+
+    if @idea.save
+      flash[:success] = "Idea created!"
+      redirect_to @idea
+    else
+      render :new
+    end
+  end
+
+  protected
+
+    def get_idea
+      @idea = Idea.find(params[:id])
+    end
+
+    def idea_params
+      params.require(:idea).permit(:title, :description)
+    end
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,7 @@
 class Idea < ActiveRecord::Base
+  belongs_to :user
+
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 140 }
+  validates :description, presence: true
 end
