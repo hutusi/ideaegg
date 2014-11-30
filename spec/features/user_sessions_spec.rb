6fdcee15d59f494b103f4df8e42ae7b718d0777d
John Hu <huziyong@gmail.com>
2014-11-30 16:40:46 +0800

    Add feature tests for sign in

--- spec/features/user_sessions_spec.rb
+++ spec/features/user_sessions_spec.rb
@@ -12,7 +12,37 @@ feature "UserSessions" do
     fill_in 'Password', with: user.password
     fill_in 'Password confirmation', with: user.password
     click_button 'Sign up'
-    
+
     expect(page).to have_content 'Sign out'
+  end
+
+  scenario 'User sign in with email' do
+    user = FactoryGirl.create(:user)
+
+    visit root_path
+    first(:link, 'Sign in').click
+    fill_in 'Login', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign in'
+
+    expect(page).to have_content 'Sign out'
+
+    first(:link, 'Sign out').click
+    expect(page).to have_content 'Sign in'
+  end
+
+  scenario 'User sign in with username' do
+    user = FactoryGirl.create(:user)
+
+    visit root_path
+    first(:link, 'Sign in').click
+    fill_in 'Login', with: user.username
+    fill_in 'Password', with: user.password
+    click_button 'Sign in'
+
+    expect(page).to have_content 'Sign out'
+
+    first(:link, 'Sign out').click
+    expect(page).to have_content 'Sign in'
   end
 end
