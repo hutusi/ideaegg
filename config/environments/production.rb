aea71196d399e04790dbc90af651cedd75abe56f
John Hu <huziyong@gmail.com>
2014-12-03 19:57:59 +0800

    Config mailer to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # devise configuration
+  config.action_mailer.default_url_options = { :host => 'ideaegg-new.herokuapp.com' }
+  # ActionMailer Config
+  # Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
